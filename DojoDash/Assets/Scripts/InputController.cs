c84e52a8d6495ec439b349e0a056e195a4d9c192
Theodor Risager <theodor349@gmail.com>
2019-05-20 13:09:19 +0200

    Input works again

--- DojoDash/Assets/Scripts/InputController.cs
+++ DojoDash/Assets/Scripts/InputController.cs
@@ -2,30 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Experimental.Input;
-using static PlayerControls;
 
-public class InputController : MonoBehaviour, IPlayer1Actions, IPlayer2Actions {
+public class InputController : MonoBehaviour {
 
-    [SerializeField] private PlayerControls playerControls;
+    private Vector2 dirP1 = Vector2.zero;
+    private Vector2 dirP2 = Vector2.zero;
+    private bool isChargingP1;
+    private bool isChargingP2;
 
-    private void Awake()
-    {
-        playerControls.Player1.SetCallbacks(this);
-    }
-
-    private void OnEnable()
-    {
-        playerControls.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerControls.Disable();
-    }
 
     private void Update()
     {
+
+        // Set values form keyboard
+        dirP1 = new Vector2(
+            Input.GetAxisRaw("HorizontalP1"),
+            Input.GetAxisRaw("VerticalP1")
+            );
+        dirP2 = new Vector2(
+            Input.GetAxisRaw("HorizontalP2"),
+            Input.GetAxisRaw("VerticalP2")
+            );
+        isChargingP1 = 0 != Input.GetAxisRaw("ChargeDashP1");
+        isChargingP2 = 0 != Input.GetAxisRaw("ChargeDashP2");
+
+        // Overide player 1 if there is a controller
+        if (Input.GetJoystickNames().Length >= 1)
+        {
+            dirP1 = new Vector2(
+                Input.GetAxisRaw("JoystickHorizontalP1"),
+                Input.GetAxisRaw("JoystickVerticalP1")
+                );
+            isChargingP1 = 0 != Input.GetAxisRaw("JoystickChargeDashP1");
+        }
+
+        // Overide player 2 if there is a controller
+        if (Input.GetJoystickNames().Length >= 2)
+        {
+            dirP2 = new Vector2(
+                Input.GetAxisRaw("JoystickHorizontalP2"),
+                Input.GetAxisRaw("JoystickVerticalP2")
+                );
+            isChargingP2 = 0 != Input.GetAxisRaw("JoystickChargeDashP2");
+        }
+
         //exit game (press escape)
         if (Input.GetKeyDown(KeyCode.Escape)) //TODO note: the key is hard-coded
         {
@@ -34,9 +54,6 @@ public class InputController : MonoBehaviour, IPlayer1Actions, IPlayer2Actions {
     }
 
     //player 1
-    private Vector2 dirP1 = Vector2.zero;
-    private bool isChargingP1;
-
     public bool IsPlayer1Charging()
     {
         return isChargingP1;
@@ -47,20 +64,7 @@ public class InputController : MonoBehaviour, IPlayer1Actions, IPlayer2Actions {
         return dirP1;
     }
 
-    public void OnMovementP1(InputAction.CallbackContext context)
-    {
-        dirP1 = context.ReadValue<Vector2>();
-    }
-
-    public void OnDashChargeP1(InputAction.CallbackContext context)
-    {
-        isChargingP1 = context.ReadValue<bool>();
-    }
-
     //player 2
-    private Vector2 dirP2 = Vector2.zero;
-    private bool isChargingP2;
-
     public bool IsPlayer2Charging()
     {
         return isChargingP2;
@@ -71,15 +75,6 @@ public class InputController : MonoBehaviour, IPlayer1Actions, IPlayer2Actions {
         return dirP2;
     }
 
-    public void OnMovementP2(InputAction.CallbackContext context)
-    {
-        dirP2 = context.ReadValue<Vector2>();
-    }
-
-    public void OnDashChargeP2(InputAction.CallbackContext context)
-    {
-        isChargingP2 = context.ReadValue<bool>();
-    }
 }
 
 
